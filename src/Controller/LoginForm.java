92162cafea35115833e6a0a625af4db00f0e9cb9
Ike Lee <ikedevelopmentdesign@gmail.com>
2022-06-20 17:19:52 -0500

    Updated Login Controller

--- src/Controller/LoginForm.java
+++ src/Controller/LoginForm.java
@@ -1,8 +1,34 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginForm {
-    public void onActionLogin(ActionEvent actionEvent) {
+    @javafx.fxml.FXML
+    private Label timeZoneLabel;
+    @javafx.fxml.FXML
+    private Label loginTimeZone;
+    @javafx.fxml.FXML
+    private Label loginGreeting;
+    @javafx.fxml.FXML
+    private TextField userIdField;
+    @javafx.fxml.FXML
+    private Button loginButton;
+    @javafx.fxml.FXML
+    private TextField passwordField;
+    @javafx.fxml.FXML
+    private Label userIdLabel;
+    @javafx.fxml.FXML
+    private Label passwordLabel;
+
+    @FXML
+    public void onActionLogin() throws IOException {
+        onActionLogin();
     }
+
 }
