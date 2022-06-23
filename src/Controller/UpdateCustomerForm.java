79ffbd4b8d5a30ac5367e80591e961258e111030
Ike Lee <ikedevelopmentdesign@gmail.com>
2022-06-23 13:44:54 -0500

    Added in Customer Update Model and controller

---
+++ src/Controller/UpdateCustomerForm.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class UpdateCustomerForm {
+    @javafx.fxml.FXML
+    private TextField cusNameField;
+    @javafx.fxml.FXML
+    private TextField cusPhoneField;
+    @javafx.fxml.FXML
+    private ComboBox cusCountryBox;
+    @javafx.fxml.FXML
+    private TextField cusPostalField;
+    @javafx.fxml.FXML
+    private TextField cusIDField;
+    @javafx.fxml.FXML
+    private TextField cusAddressField;
+    @javafx.fxml.FXML
+    private ComboBox cusDivisionBox;
+
+    @javafx.fxml.FXML
+    public void onActionCancel(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionUpdateCustomer(ActionEvent actionEvent) {
+    }
+}
