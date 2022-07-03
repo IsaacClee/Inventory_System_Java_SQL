f4213f116a361dd8c862ab08690cc6e065992d3e
Ike Lee <ikedevelopmentdesign@gmail.com>
2022-07-03 16:49:28 -0500

    Updated Controller for Reports

--- src/Controller/Reports.java
+++ src/Controller/Reports.java
@@ -1,17 +1,60 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 
 public class Reports {
-    public void onActionSelectContact(ActionEvent actionEvent) {
-    }
+    @javafx.fxml.FXML
+    private Label totalCustomers;
+    @javafx.fxml.FXML
+    private TableColumn scheduleEndCol;
+    @javafx.fxml.FXML
+    private TableColumn scheduleCustomerIDCol;
+    @javafx.fxml.FXML
+    private TableColumn scheduleDescriptionCol;
+    @javafx.fxml.FXML
+    private ComboBox scheduleContactBox;
+    @javafx.fxml.FXML
+    private Label divisionName;
+    @javafx.fxml.FXML
+    private TableColumn scheduleTitleCol;
+    @javafx.fxml.FXML
+    private Label totalAppointments;
+    @javafx.fxml.FXML
+    private TableColumn scheduleTypeCol;
+    @javafx.fxml.FXML
+    private ComboBox typeSelectBox;
+    @javafx.fxml.FXML
+    private TableColumn scheduleIDCol;
+    @javafx.fxml.FXML
+    private ComboBox monthSelectBox;
+    @javafx.fxml.FXML
+    private TableColumn scheduleStartCol;
+    @javafx.fxml.FXML
+    private ComboBox firstLevelDivisionSelectBox;
+    @javafx.fxml.FXML
+    private Button returnButton;
 
-    public void onActionSelectMonth(ActionEvent actionEvent) {
-    }
-
+    @javafx.fxml.FXML
     public void onActionSelectType(ActionEvent actionEvent) {
     }
 
+    @javafx.fxml.FXML
+    public void onActionSelectContact(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
     public void onActionSelectDivision(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionSelectMonth(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onActionReturn(ActionEvent actionEvent) {
     }
 }
