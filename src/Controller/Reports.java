bfe13b59b9dcc9c82dc262564718604e128dd747
LabUser <ilee23@student.wgu.edu>
2022-07-04 01:44:34 +0400

    Finished Reports GUI

--- src/Controller/Reports.java
+++ src/Controller/Reports.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class Reports {
+    public void onActionSelectContact(ActionEvent actionEvent) {
+    }
+
+    public void onActionSelectMonth(ActionEvent actionEvent) {
+    }
+
+    public void onActionSelectType(ActionEvent actionEvent) {
+    }
+
+    public void onActionSelectDivision(ActionEvent actionEvent) {
+    }
 }
